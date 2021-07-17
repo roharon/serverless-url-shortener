3c5276bdb470de9cacda2026a55a725603ad2ca1
roharon <roharon98@gmail.com>
2021-07-17 23:44:43 +0900

    Modify: forward api response with redirect_url

--- forward_shorten_url/lambda_function.py
+++ forward_shorten_url/lambda_function.py
@@ -10,14 +10,18 @@ def lambda_handler(event, context):
     shorten_key = "url/" + event.get("pathParameters").get("key")
 
     s3 = boto3.client('s3', config=Config(signature_version='s3v4'))
-    resp = s3.head_object(Bucket=S3_BUCKET, Key=shorten_key)
 
-    redirect_url = resp.get('WebsiteRedirectLocation')
+    try:
+        resp = s3.head_object(Bucket=S3_BUCKET, Key=shorten_key)
+        redirect_url = resp.get('WebsiteRedirectLocation')
+    except Exception as e:
+        print(e)
+        redirect_url = os.environ["HOST"]
 
     return {
         "statusCode": 301,
         "headers": {
-          "Location": redirect_url
+            "Location": redirect_url
         },
         "isBase64Encoded": False,
     }
