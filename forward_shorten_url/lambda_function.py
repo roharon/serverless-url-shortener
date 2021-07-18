2b97c206993cfc6cba1d2fc6ddfa2fc3947ef915
roharon <roharon98@gmail.com>
2021-07-18 21:14:39 +0900

    Refactor: forward function

--- forward_shorten_url/lambda_function.py
+++ forward_shorten_url/lambda_function.py
@@ -7,11 +7,10 @@ S3_BUCKET = os.environ['S3_BUCKET']
 
 def lambda_handler(event, context):
     print(event)
-    shorten_key = "url/" + event.get("pathParameters").get("key")
-
     s3 = boto3.client('s3', config=Config(signature_version='s3v4'))
 
     try:
+        shorten_key = "url/" + event.get("pathParameters").get("key")
         resp = s3.head_object(Bucket=S3_BUCKET, Key=shorten_key)
         redirect_url = resp.get('WebsiteRedirectLocation')
     except Exception as e:
